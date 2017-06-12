b25e8bf84b54979fb0b0abd491779109bae8d2e8
Mone Rivas <mone.rivas@gmail.com>
2017-06-12 15:27:38 -0500

    funcionalidad completa sin estilos

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ var cargarPagina = function () {
 };
 
 var cargarPersonajes = function () {
-    var url = "http://pokeapi.co/api/v2/pokemon-species/1/";
+    var url = "http://pokeapi.co/api/v2/pokemon-species/";
     $.getJSON(url, function (response) {
         var personajes = response.results;
         var total = response.count;
         mostrarTotalPersonajes(total);
         mostrarPersonajes(personajes);
     });
-
+   
 };
 
 var mostrarTotalPersonajes = function (total) {
@@ -23,27 +23,39 @@ var mostrarPersonajes = function (personajes) {
     var $ul = $("#personajes");
     personajes.forEach(function (personaje) {
         var $li = $("<li/>");
-        $li.text(personaje.name + personaje.height + "cm");
+        $li.text(personaje.name); /*Aqui le puedo concatenar las propiedades*/
         $li.addClass("personaje");
-        $li.attr("data-url", personaje.homeworld);
+        $li.attr("data-url", personaje.url);
         $ul.append($li);
-        console.log(personaje.name);
+        console.log(personaje.name + "  --  "+ personaje.url);
     });
 }
 
 
-var plantillaPlaneta = "   <h2>Planeta</h2>" +
-    "<p><strong>Nombre:</strong>__Nombre__ </p>" +
-    "<p><strong>clima:</strong>__clima__</p>"
+/*------------Plantilla--------------*/
+
+var plantillaPlaneta = "   <h2>Pokemon</h2>" +
+    "<p><strong>Nombre:</strong>__name__ </p>" +
+    "<p><strong>Colo:</strong>__color__</p>"+
+    "<p><strong>Forma:</strong>__shape__</p>"+
+    "<p><strong>Habitad:</strong>__habitat__</p>"+
+    "<p><strong>Genera:</strong>__genera__</p>"  
+    ;
+$(plantillaPlaneta).addClass("modal");
 
 var mostarDetallePersonaje = function () {
     var url = $(this).data("url");
     var $planetacontenedor = $("#planeta");
     $.getJSON(url, function (response) {
-            $planetacontenedor.html(
-                plantillaPlaneta.replace("__Nombre__", response.name).replace("__clima__", response.climate)
-            );
-            });
+        var name= response.name;
+        var color= response.color.name;
+        var shape= response.shape.name;
+        var habitat= response.habitat.name;
+        var genera= response.genera[0].genus;
+        $planetacontenedor.html(
+            plantillaPlaneta.replace("__name__", name).replace("__color__", color).replace("__shape__", shape) .replace("__habitat__", habitat).replace("__genera__", genera)
+        );
+    });
 
 };
 
