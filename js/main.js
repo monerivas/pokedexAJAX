d92caa1c0c866f6c4353d8e63a29cfc2817d0d2c
Mone Rivas <mone.rivas@gmail.com>
2017-06-12 18:14:08 -0500

    imagenes en el pokedex pero no en el modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 var cargarPagina = function () {
     cargarPersonajes();
     $(document).on("click", ".personaje", mostarDetallePersonaje);
@@ -19,15 +21,24 @@ var mostrarTotalPersonajes = function (total) {
     $("#total").text(total);
 
 }
+var contadorImagen=1;
 
 var mostrarPersonajes = function (personajes) {
     var $ul = $("#personajes");
     personajes.forEach(function (personaje) {
+        var $img= $("<img/>");
         var $li = $("<li/>");
+    
         $li.text(personaje.name); /*Aqui le puedo concatenar las propiedades*/
         $li.attr("data-target", "modal1");
         $li.addClass("personaje");
         $li.attr("data-url", personaje.url);
+        
+        $img.attr("src","img/" + contadorImagen + ".jpg");
+        $img.addClass("responsive-img center-align"); //poner clase de edicion propia
+        contadorImagen++;        
+        
+        $li.append($img);
         $ul.append($li);
         console.log(personaje.name + "  --  "+ personaje.url);
     });
@@ -35,22 +46,20 @@ var mostrarPersonajes = function (personajes) {
 
 
 /*------------Plantilla--------------*/
-var $areaImpresion = $("#modal");
-
-var plantillaPlaneta = "<section id='modal1' class='modal'>"+
+/*var plantillaPlaneta = "<section id='modal1' class='modal'>"+
     "<h2>Pokemon</h2>" +
     "<p><strong>Nombre:</strong>__name__ </p>" +
     "<p><strong>Colo:</strong>__color__</p>"+
     "<p><strong>Forma:</strong>__shape__</p>"+
     "<p><strong>Habitad:</strong>__habitat__</p>"+
     "<p><strong>Genera:</strong>__genera__</p>" +
-"</section>";
+"</section>";*/
 
 /*$(plantillaPlaneta).addClass("modal");*/
 
 var mostarDetallePersonaje = function () {
     var url = $(this).data("url");
-    var $planetacontenedor = $("#planeta");
+/*    var $planetacontenedor = $("#planeta");*/
     $.getJSON(url, function (response) {
         var name= response.name;
         var color= response.color.name;
@@ -63,6 +72,7 @@ var mostarDetallePersonaje = function () {
         $("#idshape").text(shape);
         $("#idhabitat").text(habitat);
         $("#idgenera").text(genera);
+        /*$("#foto").text();*/
         
 /*        $planetacontenedor.html(           
             plantillaPlaneta.replace("__name__", name).replace("__color__", color).replace("__shape__", shape) .replace("__habitat__", habitat).replace("__genera__", genera)
