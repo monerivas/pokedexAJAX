8e2ff215dfb23c4915cf4af4a8dcb52e47c447c3
Mone Rivas <mone.rivas@gmail.com>
2017-08-20 21:58:16 -0500

    actualizando el puerto del endpoint de http a https

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ var cargarPagina = function () {
 };
 
 var cargarPersonajes = function () {
-    var url = "http://pokeapi.co/api/v2/pokemon-species/";
+    var url = "https://pokeapi.co/api/v2/pokemon-species/";
     $.getJSON(url, function (response) {
         var personajes = response.results;
         var total = response.count;
         mostrarTotalPersonajes(total);
         mostrarPersonajes(personajes);
     });
-   
+
 };
 
 var mostrarTotalPersonajes = function (total) {
@@ -28,16 +28,16 @@ var mostrarPersonajes = function (personajes) {
     personajes.forEach(function (personaje) {
         var $img= $("<img/>");
         var $li = $("<li/>");
-    
+
         $li.text(personaje.name); /*Aqui le puedo concatenar las propiedades*/
         $li.attr("data-target", "modal1");
         $li.addClass("personaje");
         $li.attr("data-url", personaje.url);
-        
+
         $img.attr("src","img/" + contadorImagen + ".jpg");
         $img.addClass("responsive-img center-align"); //poner clase de edicion propia
-        contadorImagen++;        
-        
+        contadorImagen++;
+
         $li.append($img);
         $ul.append($li);
         console.log(personaje.name + "  --  "+ personaje.url);
@@ -66,18 +66,18 @@ var mostarDetallePersonaje = function () {
         var shape= response.shape.name;
         var habitat= response.habitat.name;
         var genera= response.genera[0].genus;
-        
+
         $("#idname").text(name);
         $("#idcolor").text(color);
         $("#idshape").text(shape);
         $("#idhabitat").text(habitat);
         $("#idgenera").text(genera);
         /*$("#foto").text();*/
-        
-/*        $planetacontenedor.html(           
+
+/*        $planetacontenedor.html(
             plantillaPlaneta.replace("__name__", name).replace("__color__", color).replace("__shape__", shape) .replace("__habitat__", habitat).replace("__genera__", genera)
         );*/
-       
+
     });
 
 };
